484c9c33151109c8bc7b715d1f2a18ad6512d246
razod <masonboulier@gmail.com>
2025-02-24 16:53:59 -0500

    recipe card!!!!!!

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,44 @@
 import SearchForm from '@/components/SearchForm';
+import RecipeCard from '@/components/RecipeCard';
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
 
   const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views: 2501,
+    author: { _id: 1, name: 'Best Creamier' },
+    _id: 1,
+    description: 'Best recipe ever',
+    image: 'https://icecreamfromscratch.com/wp-content/uploads/2022/03/Cookie-Monster-Ice-Cream-1.2-720x720.jpg',
+    category: 'protein',
+    title: 'Cookie Monster Creami'
+  }]
+
   return (
     <>
       <section className="main_container">
         <h1 className="heading">Find your new favorite recipe!</h1>
         <p className="sub-heading">Post your own recipes or try new ones!</p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All recipes'}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts?.length > 0 ? (
+            posts.map((post: RecipeCardType) => (
+              <RecipeCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No recipes found</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/RecipeCard.tsx
@@ -0,0 +1,58 @@
+import { formatDate } from '@/lib/utils'
+import { EyeIcon } from 'lucide-react'
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from './ui/button';
+
+const RecipeCard = ({ post }: { post: RecipeCardType }) => {
+
+    const { _createdAt, views, author, title, category, _id, description, image } = post;
+
+    return (
+        <li className='recipe-card group'>
+            <div className="flex-between">
+                <p className="recipe-card_date">
+                    {formatDate(_createdAt)}
+                </p>
+                <div className="flex gap-1.5">
+                    <EyeIcon className='size-6 text-primary' />
+                    <span className='text-16-medium'>{views}</span>
+                </div>
+            </div>
+
+            <div className="flex-between mt-5 gap-5">
+                <div className="flex-1">
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">{author?.name}</p>
+                    </Link>
+                    <Link href={`/recipe/${_id}`}>
+                        <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+                    </Link>
+                </div>
+                <Link href={`/user/${author?._id}`}>
+                    <Image src="https://placehold.co/48x48" alt="Placeholder" width={48} height={48} className='rounded-full' />
+                </Link>
+            </div>
+            <Link href={`/recipe/${_id}`}>
+                <p className="recipe-card_desc">
+                    {description}
+                </p>
+
+                <img src={image} alt="placeholder" className='recipe-card_img' />
+            </Link>
+
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/?query=${category.toLowerCase()}`}>
+                    <p className="text-16-medium">{category}</p>
+                </Link>
+                <Button className='recipe-card_btn' asChild>
+                    <Link href={`/recipe/${_id}`}>
+                        Details
+                    </Link>
+                </Button>
+            </div>
+        </li>
+    )
+}
+
+export default RecipeCard
