9d1e78a43a36d80c9d2807cd38f17e96a3012f83
razod <masonboulier@gmail.com>
2025-02-24 18:59:06 -0500

    get params

---
+++ app/(root)/recipe/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = ({ params }: { params: { id: string }}) => {
+    return (
+        <div>RECIPE POST INFO GOES HERE...</div>
+    )
+}
+
+export default page
