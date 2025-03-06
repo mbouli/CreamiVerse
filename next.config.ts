99501688aed89fa25150c0d39b7bd734afa0072c
razod <masonboulier@gmail.com>
2025-03-06 15:44:44 -0500

    ppr setup

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
       hostname: '*',
     }],
     dangerouslyAllowSVG: true,
+  },
+  experimental: {
+    ppr: 'incremental'
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right';
   }
 };
 
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -12,4 +12,20 @@ export const RECIPES_QUERY = defineQuery(`*[_type == "recipe"  && defined(slug.c
     description,
     category,
     image
-}`) 
+}`)
+
+export const RECIPE_BY_ID_QUERY = defineQuery(`
+  *[_type == "recipe" && _id == $id][0]{
+  _id,
+  title,
+    slug,
+    _createdAt,
+    author -> {
+      _id, name, image, bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+}`);
