c30b0ef2a8d26b3a62a8be9149e968d73ad93eac
razod <masonboulier@gmail.com>
2025-05-14 17:19:32 -0400

    create write client & update view count

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,19 @@
 import { client } from '@/sanity/lib/client'
 import { RECIPE_VIEWS_QUERY } from '@/sanity/lib/queries';
+import { writeClient } from '@/sanity/lib/write-client';
 import React from 'react'
 
 const View = async ({ id }: { id: string }) => {
     const { views: totalViews } = await client
         .withConfig({ useCdn: false })
         .fetch(RECIPE_VIEWS_QUERY, { id });
+
+        await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1})
+        .commit();
+
+
     return (
         <div className="view-container">
             <p className="view-text">
